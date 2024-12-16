2e25f849d598c8d3a16b8f2776c61060b7609057
Joachim Løvgaard <joachim@loevgaard.dk>
2024-12-16 13:08:28 +0100

    Optimize ChannelContextRuntimeChecker

--- src/Checker/Runtime/ChannelContextRuntimeChecker.php
+++ src/Checker/Runtime/ChannelContextRuntimeChecker.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Setono\SyliusCatalogPromotionPlugin\Checker\Runtime;
 
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\Common\Collections\Selectable;
 use Setono\SyliusCatalogPromotionPlugin\Model\PromotionInterface;
 use Sylius\Component\Channel\Context\ChannelContextInterface;
 
@@ -15,6 +17,13 @@ final class ChannelContextRuntimeChecker implements RuntimeCheckerInterface
 
     public function isEligible(PromotionInterface $catalogPromotion): bool
     {
-        return $catalogPromotion->getChannels()->contains($this->channelContext->getChannel());
+        $channel = $this->channelContext->getChannel();
+
+        $collection = $catalogPromotion->getChannels();
+        if ($collection instanceof Selectable) {
+            return $collection->matching(Criteria::create()->andWhere(Criteria::expr()->eq('code', $channel->getCode())))->count() > 0;
+        }
+
+        return $collection->contains($channel);
     }
 }
