8219a6d4cf55212cf421279752bf89dc1b3b08f8
Joachim Løvgaard <joachim@loevgaard.dk>
2024-12-20 15:24:05 +0100

    Fix dep issues

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -6,4 +6,6 @@ use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
     ->ignoreErrorsOnPackage('fakerphp/faker', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('psr/clock-implementation', [ErrorType::UNUSED_DEPENDENCY])
+    ->ignoreErrorsOnPackage('symfony/clock', [ErrorType::UNUSED_DEPENDENCY])
 ;
