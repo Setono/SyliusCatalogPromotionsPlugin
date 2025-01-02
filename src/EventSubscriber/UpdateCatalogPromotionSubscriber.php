ac3e4fa3830a9c2aff2dad79496ba26c91d5f9df
Joachim Løvgaard <joachim@loevgaard.dk>
2025-01-02 14:43:55 +0100

    Handle catalog promotion updates

---
+++ src/EventSubscriber/UpdateCatalogPromotionSubscriber.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\SyliusCatalogPromotionPlugin\EventSubscriber;
+
+use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Setono\SyliusCatalogPromotionPlugin\Message\Command\StartCatalogPromotionUpdate;
+use Setono\SyliusCatalogPromotionPlugin\Model\PromotionInterface;
+use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Contracts\Service\ResetInterface;
+use Webmozart\Assert\Assert;
+
+/**
+ * Notice that we don't need to handle the removal of catalog promotions because although the catalog promotion
+ * might be pre-qualified, it will not be applied to any products (because it doesn't exist anymore)
+ */
+final class UpdateCatalogPromotionSubscriber implements EventSubscriberInterface, ResetInterface
+{
+    /**
+     * A list of catalog promotions to update
+     *
+     * @var list<PromotionInterface>
+     */
+    private array $catalogPromotions = [];
+
+    public function __construct(private readonly MessageBusInterface $commandBus)
+    {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'setono_sylius_catalog_promotion.promotion.post_create' => 'update',
+            'setono_sylius_catalog_promotion.promotion.post_update' => 'update',
+            KernelEvents::TERMINATE => 'dispatch',
+            ConsoleEvents::TERMINATE => 'dispatch',
+        ];
+    }
+
+    public function update(ResourceControllerEvent $event): void
+    {
+        $obj = $event->getSubject();
+        Assert::isInstanceOf($obj, PromotionInterface::class);
+
+        $this->catalogPromotions[] = $obj;
+    }
+
+    public function postPersist(LifecycleEventArgs $eventArgs): void
+    {
+        $obj = $eventArgs->getObject();
+        if (!$obj instanceof PromotionInterface) {
+            return;
+        }
+
+        $this->catalogPromotions[] = $obj;
+    }
+
+    public function postUpdate(LifecycleEventArgs $eventArgs): void
+    {
+        $obj = $eventArgs->getObject();
+        if (!$obj instanceof PromotionInterface) {
+            return;
+        }
+
+        $this->catalogPromotions[] = $obj;
+    }
+
+    public function dispatch(): void
+    {
+        if ([] === $this->catalogPromotions) {
+            return;
+        }
+
+        $this->commandBus->dispatch(new StartCatalogPromotionUpdate(catalogPromotions: $this->catalogPromotions));
+    }
+
+    public function reset(): void
+    {
+        $this->catalogPromotions = [];
+    }
+}
