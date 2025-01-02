0520cba7517159050e1509d19ef2e98af8748cde
Joachim Løvgaard <joachim@loevgaard.dk>
2025-01-02 10:44:52 +0100

    Fix formatting

--- src/Resources/public/js/app.js
+++ src/Resources/public/js/app.js
@@ -1,9 +1,9 @@
 (function ($) {
-  $(function () {
-    $('#rules a[data-form-collection="add"]').on('click', function () {
-      setTimeout(function () {
-        $('select[name^="setono_sylius_catalog_promotion_promotion[rules]"][name$="[type]"]').last().change();
-      }, 50);
+    $(function () {
+        $('#rules a[data-form-collection="add"]').on('click', function () {
+            setTimeout(function () {
+                $('select[name^="setono_sylius_catalog_promotion_promotion[rules]"][name$="[type]"]').last().change();
+            }, 50);
+        });
     });
-  });
 })(jQuery);
