209944e96860d1f2a25eb40a30588ddd5770ceb1
Joachim Løvgaard <joachim@loevgaard.dk>
2025-01-09 12:12:32 +0100

    Fixing false positives

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -6,4 +6,7 @@ use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
     ->ignoreErrorsOnPackage('fakerphp/faker', [ErrorType::SHADOW_DEPENDENCY])
+    ->ignoreErrorsOnPackage('twig/extra-bundle', [ErrorType::UNUSED_DEPENDENCY]) // We use this to register the StringExtension
+    ->ignoreErrorsOnPackage('twig/string-extra', [ErrorType::UNUSED_DEPENDENCY]) // This is the StringExtension that we use for the 'u' filter
+    ->ignoreErrorsOnPackage('twig/twig', [ErrorType::UNUSED_DEPENDENCY]) // Obviously we use this
 ;
