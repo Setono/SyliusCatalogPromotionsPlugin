2e77d91d8812bd5e5a20fa5cff7934144a6415b4
Joachim Løvgaard <joachim@loevgaard.dk>
2025-01-09 11:38:14 +0100

    Use AsCommand attribute

--- src/Command/UpdateCommand.php
+++ src/Command/UpdateCommand.php
@@ -5,17 +5,18 @@ declare(strict_types=1);
 namespace Setono\SyliusCatalogPromotionPlugin\Command;
 
 use Setono\SyliusCatalogPromotionPlugin\Message\Command\StartCatalogPromotionUpdate;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 
+#[AsCommand(
+    name: 'setono:sylius-catalog-promotion:update',
+    description: 'Run a catalog promotion update',
+)]
 final class UpdateCommand extends Command
 {
-    protected static $defaultName = 'setono:sylius-catalog-promotion:update';
-
-    protected static $defaultDescription = 'Run a catalog promotion update';
-
     public function __construct(private readonly MessageBusInterface $commandBus)
     {
         parent::__construct();
@@ -23,7 +24,7 @@ final class UpdateCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->commandBus->dispatch(new StartCatalogPromotionUpdate(triggeredBy: sprintf('Running command "%s"', (string) self::$defaultName)));
+        $this->commandBus->dispatch(new StartCatalogPromotionUpdate(triggeredBy: sprintf('Running command "%s"', (string) self::getDefaultName())));
 
         return 0;
     }
